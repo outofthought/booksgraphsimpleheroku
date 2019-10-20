467dea13c2b82dd58476c132f25d21853f79be2b
Katie Lamber <katielamber02@gmail.com>
2019-10-20 23:30:59 +0300

    accim first try

---
+++ server.js
@@ -0,0 +1,44 @@
+// Importing Modules
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// importing files
+const routes = require("./routes");
+
+// Define Global Variables
+const app = express();
+const log = console.log;
+const PORT = process.env.PORT || 8080; // Step 1
+
+// Step 2
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://katielamber02:begginYou6342@ds161740.mlab.com:61740/deploy1",
+
+  {
+    useNewUrlParser: true
+  }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+// Configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", routes);
+
+// Step 3
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+  });
+}
+
+app.listen(PORT, () => {
+  log(`Server is starting at PORT: ${PORT}`);
+});
